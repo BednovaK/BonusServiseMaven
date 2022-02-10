5f7fe21ad0d43a907a6a3cb7a0d307aca0e61adf
Bednova Ekaterina <96745291+BednovaK@users.noreply.github.com>
2022-02-10 12:09:55 +0300

    Version changed

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BonusServiceTest {
+public class BonusServiceTest {
 
     @Test
     void shouldCalculateForRegisteredAndUnderLimit() {
